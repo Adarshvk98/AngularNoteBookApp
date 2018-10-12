075c69e991eaf298362b63ede43ec3b848768d86
Adarsh vk <33983977+Adarshvk98@users.noreply.github.com>
2018-10-12 05:55:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('./dist/NoteBookApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname ,'/dist/NoteBookApp/index.html'));
+res.sendFile('./dist/NoteBookApp/index.html');
 });
 
 // Start the app by listening on the default Heroku port
