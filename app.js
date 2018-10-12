f339217e2005e8b90efdaca846496f473d617844
Adarsh vk <33983977+Adarshvk98@users.noreply.github.com>
2018-10-12 05:33:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname,'/dist/NoteBookApp')));
+app.use(express.static('./dist/NoteBookApp'));
 
 app.get('/*', function(req,res) {
     
